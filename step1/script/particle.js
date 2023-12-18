ed7dee9c4808aebd3c2e75e6a27c142603c47386
sungeunpark0416 <144648524+sungeunpark0416@users.noreply.github.com>
2023-12-18 13:19:33 +0900

    4

--- step1/script/particle.js
+++ step1/script/particle.js
@@ -21,13 +21,11 @@ class Particle {
 
     let totalForce = createVector(0, 0);
 
-    // Change color based on the distance to the mouse
     if (distanceToMouse < 200) {
       let repulsionForce = map(distanceToMouse, 0, 200, MAX_FORCE, MIN_FORCE);
       fromMouseToParticle.setMag(repulsionForce);
       totalForce.add(fromMouseToParticle);
 
-      // Gradient effect based on the distance
       let gradientFactor = map(distanceToMouse, 0, 200, 0, 1);
       let gradientColor = lerpColor(
         color('lightgray'),
@@ -36,7 +34,6 @@ class Particle {
       );
       this.color = gradientColor;
     } else {
-      // Reset the color to the base color when the mouse is not close
       this.color = this.baseColor;
     }
 
@@ -52,7 +49,7 @@ class Particle {
 
   draw() {
     fill(this.color);
-    // stroke('white');
+
     noStroke();
     rect(this.x, this.y, PARTICLE_SIZE);
   }
